97fa8692dfed859136508683ca0e6ce109907696
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-09-16 21:36:39 +0000

    init

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# manifest creates the file "/tmp/school"
+
+file { '/tmp/school':
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
