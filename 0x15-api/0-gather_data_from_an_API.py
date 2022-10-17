564dbf02d621289ed27611d65c02f611555d8027
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-10-17 20:18:10 +0000

    add readme and script 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" script gets the information of user from
+https://jsonplaceholder.typicode.com/"""
+
+
+import requests
+import sys
+
+
+url_prefix = "https://jsonplaceholder.typicode.com/users/"
+
+
+def main():
+    """ main func """
+    id_ = sys.argv[1]
+    todos_url = url_prefix + id_ + "/todos"
+    users_url = url_prefix + id_
+    todos = requests.get(todos_url)
+    todos_list = todos.json()
+    size = len(todos_list)
+    completed = 0
+    completed_tasks_title = ""
+    for item in todos_list:
+        if item.get("completed"):
+            completed += 1
+            completed_tasks_title = completed_tasks_title + "\t" +\
+                item.get("title") + "\n"
+
+    ratio = str(completed) + "/" + str(size)
+    users = requests.get(users_url)
+    name = users.json().get("name")
+
+    if (size):
+        print("Employee {} is done with tasks({}):".format(name, ratio))
+    print(completed_tasks_title, end="")
+
+
+if __name__ == "__main__":
+    main()
